5b4c8faec8224138f7ca5689265831ce26115e43
Daye Kim <dykim315@naver.com>
2023-11-12 16:44:17 +0900

    [Feat] #2 - 1주차 과제

--- iOS-Animation Study/Extension/UIButton+.swift
+++ iOS-Animation Study/Extension/UIButton+.swift
@@ -9,11 +9,11 @@ import UIKit
 
 extension UIButton {
     func shakeButton() {
-        self.transform = CGAffineTransform(translationX: 0, y: 20)
+        self.transform = CGAffineTransform(translationX: 0, y: 5)
         UIView.animate(withDuration: 1,
-                       delay: 0,
-                       usingSpringWithDamping: 0.2,
-                       initialSpringVelocity: 1,
+                               delay: 0,
+                               usingSpringWithDamping: 0.1,
+                       initialSpringVelocity: 2,
                        options: [.curveEaseInOut]) {
             self.transform = .identity
         }
