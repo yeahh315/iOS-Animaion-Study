b412061bb515943807f4bed48f6cf4dec9d1ec70
Daye Kim <dykim315@naver.com>
2023-12-15 16:50:01 +0900

    [Setting] #7 - 세팅

---
+++ iOS-Animation Study/Final/Fighting/FinalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FinalViewController.swift
+//  iOS-Animation Study
+//
+//  Created by 김다예 on 12/15/23.
+//
+
+import UIKit
+
+final class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
